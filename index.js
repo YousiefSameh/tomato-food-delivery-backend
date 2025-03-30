58ce974847ecdf94c1f4b66b8e7398b0d6abc7fa
Yousief Sameh <yousief.sameh@outlook.com>
2025-03-31 00:27:10 +0200

    Fix Images

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/api/food", foodRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/user", userRouter);
-app.use("/image", express.static("uploads"));
+app.use("/image", express.static("./uploads"));
 
 app.get("/", (req, res) => res.status(200).send("API Working !"));
 
